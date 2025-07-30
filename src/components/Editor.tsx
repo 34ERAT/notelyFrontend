598c696aa22b526189e652b2b3cf75618e9d0803
34ERAT <philipkanuti@gmail.com>
2025-07-30 22:19:46 +0300

    setup bubble menu

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,14 +2,19 @@ import { useNoteStore } from "../store";
 import { EditorContent, useEditor } from "@tiptap/react";
 import "./TipTap.scss";
 import StarterKit from "@tiptap/starter-kit";
+import { Placeholder } from "@tiptap/extensions";
 import SaveNoteBtn from "./Editor/SaveNoteBtn";
 import EditNoteBtn from "./Editor/EditNoteBtn";
 import { Box } from "@mui/material";
 import EditorToolbar from "./Editor/EditorToolbar";
 import { useEffect } from "react";
 import BubbleMenu from "./Editor/BubbleMenu";
-import FloatingMenu from "./Editor/FloatingMenu";
-const extensions = [StarterKit];
+const extensions = [
+  StarterKit,
+  Placeholder.configure({
+    placeholder: "Write somethig .....",
+  }),
+];
 function Editor({ mode }: { mode: "edit" | "new" }) {
   const { note, setNote } = useNoteStore();
   const { initalContent } = useNoteStore();
@@ -18,6 +23,7 @@ function Editor({ mode }: { mode: "edit" | "new" }) {
     content: "",
     onUpdate({ editor }) {
       setNote({ ...note, content: editor.getHTML() });
+      console.log(editor.getHTML());
     },
   });
   useEffect(() => {
@@ -26,7 +32,7 @@ function Editor({ mode }: { mode: "edit" | "new" }) {
   return (
     <Box>
       <EditorToolbar />
-      <FloatingMenu editor={editor} />
+      {/* <FloatingMenu editor={editor} /> */}
       <BubbleMenu editor={editor} />
       <EditorContent editor={editor} />
       {mode == "new" ? <SaveNoteBtn /> : <EditNoteBtn />}
